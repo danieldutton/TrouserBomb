bec82fdfd05143404e10b9a5115e30201b509afe
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-05 19:18:11 +0100

    Initial Commit

---
+++ TrouserBomb/TrouserBombApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrouserBombApp.swift
+//  TrouserBomb
+//
+//  Created by Daniel Dutton on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrouserBombApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
