5e22e93eeb57ae5be4271137d7a7b4f7a44d0a7b
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-05 22:17:56 +0100

    Add unstyled NavigationView

--- TrouserBomb/View/TrouserBombView.swift
+++ TrouserBomb/View/TrouserBombView.swift
@@ -4,24 +4,30 @@ import SwiftUI
 struct TrouserBombView: View {
     var sandbox = SandBox()
     var body: some View {
-        ZStack {
-            Color("Blue_Background")
-            VStack {
-                Spacer()
-                Button(action: {
-                    sandbox.fart()
-                }, label: {
-                    Text("RANDOM RIPPER")
-                        .foregroundColor(.white)
-                })
-                .frame(maxWidth: .infinity)
-                .padding()
-                .onShake {
-                    sandbox.fart()
+        NavigationView {
+            ZStack {
+                Color("Blue_Background")
+                VStack {
+                    Spacer()
+                    Button(action: {
+                        sandbox.fart()
+                    }, label: {
+                        Text("RANDOM RIPPER")
+                            .foregroundColor(.white)
+                    })
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .onShake {
+                        sandbox.fart()
+                    }
+                    .background(Color("Blue_Random_Ripper"))
                 }
-                .background(Color("Blue_Random_Ripper"))
+                .navigationBarTitle("Trouser Bomb")
+                .navigationBarTitleDisplayMode(.inline)
             }
-        }.edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.all)
+            
+        }
     }
 }
 
